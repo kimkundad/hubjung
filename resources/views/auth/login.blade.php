74468a138d40e4c279d0b91e0b37f08ff5ca7e4a
kim <kim.kundad@gmail.com>
2019-10-22 20:45:52 +0700

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,104 +1,163 @@
-@extends('layouts.template')
-@section('stylesheet')
-
-
-@stop('stylesheet')
+@extends('layouts.app')
 @section('content')
+<style>
+body{
+      background-color: #f5f5f5;
+}
+.ui.button {
+  width: 100%;
+  text-decoration: none;
+    cursor: pointer;
+    display: inline-block;
+    min-height: 1em;
+    outline: 0;
+    border: none;
+    background: #e0e1e2;
+    color: #fff;
+    margin: 0 .25em 0 0;
+    padding: .78571429em 1.5em;
+    text-shadow: none;
+    font-weight: 700;
+    line-height: 1em;
+    font-style: normal;
+    text-align: center;
+    border-radius: .28571429rem;
+    user-select: none;
+    -webkit-transition: opacity .1s ease,background-color .1s ease,color .1s ease,box-shadow .1s ease,background .1s ease;
+    transition: opacity .1s ease,background-color .1s ease,color .1s ease,box-shadow .1s ease,background .1s ease;
+    will-change: '';
+}
+.ui.facebook.button {
+    background-color: #3b5998;
+    text-shadow: none;
+}
+.ui.facebook.button:hover {
+    background-color: #334d84;
+    text-shadow: none;
+}
+.ui.facebook.button, .ui.google.plus.button, .ui.instagram.button, .ui.pinterest.button, .ui.twitter.button, .ui.vk.button, .ui.youtube.button {
+    background-image: none;
+    box-shadow: 0 0 0 0 rgba(34,36,38,.15) inset;
+    color: #fff;
+}
+.panel-default>.panel-heading {
+    background-image: url({{url('assets/image/login_bg.png')}});
 
-<!-- ================================
-    START BREADCRUMB AREA
-================================= -->
-<section class="breadcrumb-area">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="breadcrumb-content">
-                    <h2 class="breadcrumb__title">login</h2>
+}
+.panel-heading {
+    padding: 5px 5px;
+}
+.login_box {
 
-                </div><!-- end breadcrumb-content -->
-            </div><!-- end col-lg-12 -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end breadcrumb-area -->
-<!-- ================================
-    END BREADCRUMB AREA
-================================= -->
+    margin: 56px auto;
+    padding: 15px 15px 0;
+}
+.t_mid {
+    text-align: center;
+}
+.g_right {
+  margin-top: -5px;
+    float: right;
+}
+.logo-login{
+      margin: 0 auto 20px auto;
+}
+.t_gray {
+
+    color: #9e9e9e;
+}
+.login_box .sign_up_btn, .login_box .login_btn {
+    background-color: #fff;
+    color: #424242;
+    padding: 10px 25px;
+}
+</style>
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4 ">
+            <div class="panel panel-default login_box">
+
+                <div class="panel-body">
+
+                  <div class="col-md-12 text-center">
+                    <h3>เข้าสู่ระบบ</h3>
+                    <br />
+                  </div>
+
+                  <!--  <div class="form-group">
+
+                    <div style="margin-bottom: 16px;">
+                    <a href="redirect" class=" ui facebook fluid button"><i class="fa fa-facebook icon-fa " style=""></i> สมัครด้วย Facebook</a>
+                  </div>
 
 
-<!-- ================================
-       START FORM AREA
-================================= -->
-<section class="form-shared">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7 mx-auto">
-                <div class="contact-form-action">
-                    <div class="form-heading text-center">
-                        <h3 class="form__title">Login to your account!</h3>
-                        <p class="form__desc">with your social network.</p>
-                    </div>
-                    <!--Contact Form-->
+                </div>
+
+
+                <div><p class="t_mid">หรือ</p></div> -->
+
+
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-6 col-xs-12 form-group">
-                                <button class="theme-btn sign-btn btn__google" >
-                                    <i class="fa fa-google"></i> Google
-                                </button>
-                            </div><!-- end col-lg-4 -->
-                            <div class="col-lg-6 col-sm-6 col-xs-12 form-group">
-                                <button class="theme-btn sign-btn btn__facebook" >
-                                    <i class="fa fa-facebook"></i> Facebook
-                                </button>
-                            </div><!-- end col-lg-4 -->
 
-                            <div class="col-lg-12 col-sm-12 col-xs-12 account-assist text-center">
-                                <p class="account__desc account__desc2">or</p>
-                            </div><!-- end col-md-12 -->
-                            <div class="col-lg-12 col-sm-12 form-group">
-                                <input class="form-control" type="text" name="email" placeholder="Email">
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+
+
+                            <div class="col-md-12">
+                                <input id="email" type="email" class="form-control" name="email" placeholder="Email" value="{{ old('email') }}">
+
                                 @if ($errors->has('email'))
-                                  <span class="help-block">
-                                      <strong>{{ $errors->first('email') }}</strong>
-                                  </span>
-                              @endif
-                                <span class="la la-user input-icon"></span>
-                            </div><!-- end col-md-12 -->
-                            <div class="col-lg-12 col-sm-12 form-group">
-                                <input class="form-control" type="text" name="password" placeholder="Password">
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+
+
+                            <div class="col-md-12">
+                                <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                                <span class="la la-lock input-icon"></span>
-                            </div><!-- end col-md-12 -->
-                            <div class="col-lg-12 col-sm-12 col-xs-12 form-condition">
-                                <div class="custom-checkbox">
-                                    <input type="checkbox" id="chb1">
-                                    <label for="chb1">Remember Me</label>
-                                    <a href="{{url('password/reset')}}" class="pass__desc"> Forgot my password?.</a>
-                                </div>
                             </div>
-                            <div class="col-lg-12 col-sm-12 col-xs-12 form-group">
-                                <button class="theme-btn" type="submit">login now</button>
-                            </div><!-- end col-md-12 -->
-                            <div class="col-lg-12 col-sm-12 col-xs-12 account-assist">
-                                <p class="account__desc">Not a member?<a href="{{url('register')}}"> Register now</a></p>
-                            </div><!-- end col-md-12 -->
-                        </div><!-- end row -->
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 ">
+
+                                    <label>
+                                        <input type="checkbox" name="remember"> จดจำฉันไว้
+                                    </label>
+
+                                <a class="btn btn-link g_right"  href="{{ url('/password/reset') }}">ลืมพาสเวิร์ด?</a>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 ">
+                                <button type="submit" class="btn btn-primary btn-block">
+                                    <i class="fa fa-btn "></i> เข้าสู่ระบบ
+                                </button>
+                            </div>
+                        </div>
+                        <hr>
+
+
+                        <label class="t_gray g_left">
+                            หากยังไม่สมัครสมาชิก
+                        </label>
+                        <a class="btn btn-default" style="float:right" href="{{url('register')}}">สมัครสมาชิก</a>
+
                     </form>
-                </div><!-- end contact-form -->
-            </div><!-- end col-md-7 -->
-        </div><!-- end row -->
-    </div><!-- end container -->
-</section><!-- end form-shared -->
-<!-- ================================
-       START FORM AREA
-================================= -->
-
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
-
-@section('scripts')
-
-@stop('scripts')
